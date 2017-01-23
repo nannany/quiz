604a78126ed13050aeaefabd382c723392931b11
nannany <ymym1990ymym@gmail.com>
2017-01-24 00:38:52 +0900

    commit ans

---
+++ src/main/java/quiz/paiza/d/trump/Main.java
@@ -0,0 +1,24 @@
+package quiz.paiza.d.trump;
+
+import java.util.Scanner;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String firstWord = sc.next();
+	String secondWord = sc.next();
+
+	BiPredicate<String,String> p = (f, s) -> f.equals("J") && s.equals("J");
+
+	Consumer<String> c = System.out::println;
+
+	if (!p.test(firstWord,secondWord)) {
+	    c.accept(firstWord + " " + secondWord);
+	} else {
+	    c.accept(firstWord + " Q");
+	}
+	sc.close();
+    }
+}
