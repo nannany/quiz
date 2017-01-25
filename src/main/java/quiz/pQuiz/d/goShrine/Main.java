41c8ed6738e71576a49626bfc812ef062056e8ed
nannany <ymym1990ymym@gmail.com>
2017-01-26 01:04:07 +0900

    クロージャ？が効いてうまくいかない。

---
+++ src/main/java/quiz/pQuiz/d/goShrine/Main.java
@@ -0,0 +1,26 @@
+package quiz.pQuiz.d.goShrine;
+
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+
+public class Main {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int L = sc.nextInt();
+	int M = sc.nextInt();
+	int N = sc.nextInt();
+
+	StringBuilder sb = new StringBuilder();
+
+	BiConsumer<Integer,String> mojiCreator = (kazu,moji) -> {
+	    for(int i= 0;i<kazu;i++){
+		sb.append(moji);
+	    }
+	};
+	mojiCreator.accept(L,"A");
+	mojiCreator.accept(M,"B");
+	mojiCreator.accept(N,"A");
+
+	System.out.println(mojiCreator.toString());
+    }
+}
