14182d8752ddf9ea178f5398b4e8308cab4c0214
nannany <ymym1990ymym@gmail.com>
2017-04-05 21:26:39 +0900

    動的計画法。正解

--- src/main/java/quiz/challengeBook/cowBowling_poj3176/Main.java
+++ src/main/java/quiz/challengeBook/cowBowling_poj3176/Main.java
@@ -1,9 +1,10 @@
 package quiz.challengeBook.cowBowling_poj3176;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    int[][] line;
+
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	int N = sc.nextInt();
@@ -15,8 +16,28 @@ public class Main {
 		line[i][j] = Integer.parseInt(tmp[j]);
 	    }
 	}
-	System.out.println(Arrays.toString(line[0]));
 
+	int dp[][] = new int[N][N];
 
+	dp[0][0] = line[0][0];
+
+	for (int i = 1; i < N; i++) {
+	    for (int j = 0; j <= i; j++) {
+		if (j == 0) {
+		    dp[i][j] = dp[i - 1][j] + line[i][j];
+		} else {
+		    dp[i][j] = Math.max(dp[i - 1][j - 1] + line[i][j], dp[i - 1][j] + line[i][j]);
+		}
+	    }
+	}
+
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+	    if (ans < dp[N - 1][i]) {
+		ans = dp[N - 1][i];
+	    }
+	}
+	System.out.println(ans);
+	sc.close();
     }
 }
