2468018a2d45f8037720d9bcfafd60c91a0e0c3f
nannany <ymym1990ymym@gmail.com>
2017-04-22 20:26:17 +0900

    エンティティ作成

--- src/main/java/marathon/codingame/Player.java
+++ src/main/java/marathon/codingame/Player.java
@@ -14,9 +14,8 @@ class Player {
 
 	// game loop
 	while (true) {
-	    int myShipCount = in.nextInt(); // the number of remaining ships
-	    int entityCount = in.nextInt(); // the number of entities (e.g.
-					    // ships, mines or cannonballs)
+	    int myShipCount = in.nextInt(); // 自分の機体数
+	    int entityCount = in.nextInt();
 
 	    int[] entityId = new int[entityCount];
 	    String[] entityType = new String[entityCount];
@@ -29,24 +28,43 @@ class Player {
 
 	    int myShipId = -1;
 
+	    List<MyShip> myShipList = new ArrayList<MyShip>();
+	    List<EnemyShip> enemyShipList = new ArrayList<EnemyShip>();
 	    List<Barrel> barrelList = new ArrayList<Barrel>();
+	    List<Cannonball> cannonballList = new ArrayList<Cannonball>();
+	    List<Mine> mineList = new ArrayList<Mine>();
 	    for (int i = 0; i < entityCount; i++) {
 		entityId[i] = in.nextInt();
-		entityType[i] = in.next();// SHIP or BARREL
+		entityType[i] = in.next();// SHIP or BARREL or CANNONBALL or
+					  // MINE
 		x[i] = in.nextInt();
 		y[i] = in.nextInt();
-		if (entityType[i].equals("BARREL")) {
-		    Barrel barrel = new Barrel(x[i], y[i]);
-		    barrelList.add(barrel);
-		}
-		arg1[i] = in.nextInt();// the ship's rotation
-				       // orientation(between 0 and 5)
-		arg2[i] = in.nextInt();// the ship's speed (between 0 and 2)
-		arg3[i] = in.nextInt();// the ship's stock of rum units
-		arg4[i] = in.nextInt();// 1 if the ship is controlled by you, 0
-				       // otherwise
+		arg1[i] = in.nextInt();
+		arg2[i] = in.nextInt();
+		arg3[i] = in.nextInt();
+		arg4[i] = in.nextInt();
 		if (arg4[i] == 1) {
 		    myShipId = entityId[i];
+		}
+		if (entityType[i].equals("SHIP") && arg4[i] == 1) {
+		    MyShip myShip = new MyShip(x[i], y[i], arg1[i], arg2[i], arg3[i]);
+		    myShipList.add(myShip);
+		}
+		if (entityType[i].equals("SHIP") && arg4[i] == 0) {
+		    EnemyShip enemyShip = new EnemyShip(x[i], y[i], arg1[i], arg2[i], arg3[i]);
+		    enemyShipList.add(enemyShip);
+		}
+		if (entityType[i].equals("BARREL")) {
+		    Barrel barrel = new Barrel(x[i], y[i], arg1[i]);
+		    barrelList.add(barrel);
+		}
+		if (entityType[i].equals("CANNONBALL")) {
+		    Cannonball cannonball = new Cannonball(x[i], y[i], arg1[i], arg2[i]);
+		    cannonballList.add(cannonball);
+		}
+		if (entityType[i].equals("MINE")) {
+		    Mine mine = new Mine(x[i], y[i]);
+		    mineList.add(mine);
 		}
 	    }
 	    int targetX = -1, targetY = -1;
@@ -67,13 +85,75 @@ class Player {
 	}
     }
 
-    static class Barrel {
-	int x;
-	int y;
+    static class MyShip extends Coordinates {
+	// 回転の向き
+	// 0:→、１:右上。。。。
+	int rotationOrientation;
+	// 速さ。0~2
+	int speed;
+	// rumのストック。100まで
+	int stock;
 
-	public Barrel(int x, int y) {
+	public MyShip(int x, int y, int rotationOrientation, int speed, int stock) {
+	    super(x, y);
+	    this.rotationOrientation = rotationOrientation;
+	    this.speed = speed;
+	    this.stock = stock;
+	}
+    }
+
+    static class EnemyShip extends Coordinates {
+	// 回転の向き
+	// 0:→、１:右上。。。。
+	int rotationOrientation;
+	// 速さ。0~2
+	int speed;
+	// rumのストック。100まで
+	int stock;
+
+	public EnemyShip(int x, int y, int rotationOrientation, int speed, int stock) {
+	    super(x, y);
+	    this.rotationOrientation = rotationOrientation;
+	    this.speed = speed;
+	    this.stock = stock;
+	}
+    }
+
+    static class Barrel extends Coordinates {
+	int amount;
+
+	public Barrel(int x, int y, int amount) {
+	    super(x, y);
+	    this.amount = amount;
+	}
+    }
+
+    static class Cannonball extends Coordinates {
+	// どの機体から発射されたか
+	int fromWhere;
+	// あと何ターンで着弾するか
+	int leftBeforeLand;
+
+	public Cannonball(int x, int y, int fromWhere, int leftBeforeLand) {
+	    super(x, y);
+	    this.fromWhere = fromWhere;
+	    this.leftBeforeLand = leftBeforeLand;
+	}
+    }
+
+    static class Mine extends Coordinates {
+	public Mine(int x, int y) {
+	    super(x, y);
+	}
+    }
+
+    static class Coordinates {
+	int x, y;
+
+	public Coordinates(int x, int y) {
 	    this.x = x;
 	    this.y = y;
 	}
+
     }
 }
