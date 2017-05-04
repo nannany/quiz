8ff88c81b963012bd0979874111cf3a8ee9b7ac8
nannany <ymym1990ymym@gmail.com>
2017-05-05 03:40:18 +0900

    次は同時に発射する実装する。

--- src/main/java/marathon/codingame/ghost_in_the_cell/Player.java
+++ src/main/java/marathon/codingame/ghost_in_the_cell/Player.java
@@ -85,18 +85,20 @@ class Player {
 		}
 	    }
 
-	    // 一番近い
 	    int minDistance = Integer.MAX_VALUE;
 	    int retFrom = 0;
 	    int retTo = 0;
 	    int retCyborgs = 0;
+
+	    // ニュートラルに送りこむ
 	    if (!neutoralFactorys.isEmpty()) {
 		int evaluateVal = Integer.MIN_VALUE;
 		for (Map.Entry<Integer, MyFactory> myFactory : myFactorys.entrySet()) {
 		    ArrayList<Edge> tmpList = graph.get(myFactory.getKey());
 		    int tmpEvaluateVal = Integer.MIN_VALUE;
 		    for (Edge edge : tmpList) {
-			if ("NEUTORAL".equals(factoryMap.get(edge.to))) {
+			if ("NEUTORAL".equals(factoryMap.get(edge.to))
+				&& getTroopSum(edge.to) <= neutoralFactorys.get(edge.to).numCyborgs) {
 			    if (evaluateVal < evaluateNeutoral(myFactory.getValue(), neutoralFactorys.get(edge.to),
 				    edge.distance)) {
 				tmpEvaluateVal = evaluateNeutoral(myFactory.getValue(), neutoralFactorys.get(edge.to),
@@ -104,6 +106,7 @@ class Player {
 				retTo = edge.to;
 				retFrom = myFactory.getKey();
 				System.err.println("retTo:" + retTo + " retFrom:" + retFrom + "dis");
+
 			    }
 			}
 		    }
@@ -111,11 +114,14 @@ class Player {
 			evaluateVal = tmpEvaluateVal;
 		    }
 		    // debug
-		    for (Edge edge : graph.get(myFactory.getKey())) {
-			System.err.println(edge.to);
-		    }
+		    // for (Edge edge : graph.get(myFactory.getKey())) {
+		    // System.err.println(edge.to);
+		    // }
 		}
-		if (myFactorys.get(retFrom).numCyborgs / 2 < neutoralFactorys.get(retTo).numCyborgs) {
+		if (neutoralFactorys.get(retTo) == null || myFactorys.get(retFrom) == null) {
+		    System.out.println("WAIT");
+		    continue flag1;
+		} else if (myFactorys.get(retFrom).numCyborgs / 2 < neutoralFactorys.get(retTo).numCyborgs) {
 		    retCyborgs = myFactorys.get(retFrom).numCyborgs / 2;
 		} else {
 		    retCyborgs = neutoralFactorys.get(retTo).numCyborgs + 10;
@@ -125,12 +131,24 @@ class Player {
 		continue flag1;
 	    }
 
+	    System.err.println("doubt");
 	    int maxCyborgsId = getIdHaveMaxCyborgs();
+	    System.err.println("maxCyborgsId:" + maxCyborgsId);
 
+	    // 敵に送り込む
 	    if (15 < myFactorys.get(maxCyborgsId).numCyborgs) {
-		for (Map.Entry<Integer, EnemyFactory> enemyFactory : enemyFactorys.entrySet()) {
-		    retTo = enemyFactory.getKey();
+		System.err.println("toEnemy");
+		int minDistanceToEnemy = Integer.MAX_VALUE;
+		flag2: for (Map.Entry<Integer, EnemyFactory> enemyFactory : enemyFactorys.entrySet()) {
+		    if (getDistanceFromMyToEnemy(maxCyborgsId, enemyFactory.getKey()) < minDistanceToEnemy) {
+			minDistanceToEnemy = getDistanceFromMyToEnemy(maxCyborgsId, enemyFactory.getKey());
+			retTo = enemyFactory.getKey();
+		    }
 		    retCyborgs = 10;
+		    if (50 <= enemyFactory.getValue().numCyborgs && 2 <= enemyFactory.getValue().numProduction) {
+			System.out.print("BOMB " + maxCyborgsId + " " + enemyFactory.getKey() + ";");
+			break flag2;
+		    }
 		}
 		System.out.println("MOVE " + maxCyborgsId + " " + retTo + " " + retCyborgs);
 		System.err.println("toEnemy:MOVE " + maxCyborgsId + " " + retTo + " " + retCyborgs);
@@ -142,14 +160,41 @@ class Player {
 	}
     }
 
+    // 敵と自分の距離を測る
+    static int getDistanceFromMyToEnemy(int myId, int enemyId) {
+	ArrayList<Edge> tmpList = graph.get(myId);
+	int ret = -1;
+	for (Edge edge : tmpList) {
+	    if (edge.to == enemyId) {
+		ret = edge.distance;
+	    }
+	}
+	return ret;
+    }
+
+    // 目的地に向かっているtroopの数を返す
+    static int getTroopSum(int destination) {
+	int ret = 0;
+	for (Map.Entry<Integer, MyTroop> myTroop : myTroops.entrySet()) {
+	    if (myTroop.getValue().to == destination) {
+		ret += myTroop.getValue().numCyborgs;
+	    }
+	}
+	return ret;
+    }
+
     // 最初ニュートラルを責める際に、どこに向かうか評価する関数
     static int evaluateNeutoral(MyFactory myFactory, NeutoralFactory neutoralFactory, int distance) {
-	return myFactory.numCyborgs - distance + neutoralFactory.numProduction * 5;
+	if (myFactory.numCyborgs == 0) {
+	    return Integer.MIN_VALUE;
+	} else {
+	    return myFactory.numCyborgs - distance + neutoralFactory.numProduction * 5;
+	}
     }
 
     // 自分の工場の中で一番サイボーグ持っているところのIDを返す。
     static int getIdHaveMaxCyborgs() {
-	int tmpMax = 0;
+	int tmpMax = -1;
 	int retId = -1;
 	for (Map.Entry<Integer, MyFactory> myFactory : myFactorys.entrySet()) {
 	    if (tmpMax < myFactory.getValue().numCyborgs) {
