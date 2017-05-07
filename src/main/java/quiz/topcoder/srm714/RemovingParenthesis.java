573e3a21ba3b92f6e4f67cfd11d8e47eb2d793e6
nannany <ymym1990ymym@gmail.com>
2017-05-08 00:38:28 +0900

    正解。

--- src/main/java/quiz/topcoder/srm714/RemovingParenthesis.java
+++ src/main/java/quiz/topcoder/srm714/RemovingParenthesis.java
@@ -1,42 +1,89 @@
 package quiz.topcoder.srm714;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RemovingParenthesis {
 
+    static int sum;
+    static Queue<String> que;
+
     public int countWays(String s) {
-	int sum = 0;
-	return solve(s, sum);
+	sum = 0;
+	que = new LinkedList<String>();
+
+	separate(s);
+	// que.stream().forEach(str -> System.out.println("que" + str));
+	int ans = 1;
+	while (!que.isEmpty()) {
+	    solve(que.poll());
+	    int tmp = sum;
+	    ans *= tmp;
+	    sum = 0;
+	}
+	return ans;
     }
 
-    static int solve(String s, int sum) {
-	int ret = 0;
+    // static void solve(String s) {
+    // if ("()".equals(s.substring(0, 2))) {
+    // solve(s.substring(2));
+    // }else if("")
+    // }
+
+    static void solve(String s) {
+	// System.out.println(s);
 	if ("".equals(s)) {
-	    ret = sum;
+	    sum++;
 	} else {
 	    if ("()".equals(s)) {
-		sum++;
-		return solve("", sum);
+		solve("");
 	    } else if ("()".equals(s.substring(0, 2))) {
-		return solve(s.substring(2), sum);
+		solve(s.substring(2));
 	    } else {
 		int parenthesisNum = s.length() / 2;
-		System.out.println(s);
-		for (int i = 0; i < parenthesisNum; i++) {
-		    int pos = s.indexOf(")", i);
+		for (int i = 1; i <= parenthesisNum; i++) {
 		    if (i == parenthesisNum - 1) {
-			System.out.println("★" + i);
-			System.out.println("parenthNum" + parenthesisNum);
-			sum++;
-			ret += solve(s.substring(1, i), sum);
+			solve(s.substring(1, s.length() - 1));
 		    } else {
-			System.out.println("★★" + i);
-			System.out.println("parenthNum" + parenthesisNum);
-
-			sum++;
-			ret += solve(s.substring(1, pos) + s.substring(pos + 1), sum);
+			int pos = getPos(s, i);
+			solve(s.substring(1, pos) + s.substring(pos + 1));
 		    }
 		}
 	    }
 	}
-	return ret;
     }
+
+    static void separate(String str) {
+	str += " ";
+	int frontNum = 0;
+	int backNum = 0;
+	for (int i = 0; i < str.length(); i++) {
+	    if ("(".equals(str.substring(i, i + 1))) {
+		frontNum++;
+	    } else {
+		backNum++;
+	    }
+	    if (frontNum == backNum) {
+		// System.out.println("★★" + str.substring(0, i + 1));
+		que.add(str.substring(0, i + 1));
+		str = str.substring(i + 1);
+		if (str.charAt(0) == ' ') {
+		    return;
+		} else {
+		    separate(str);
+		    break;
+		}
+	    }
+
+	}
+    }
+
+    static int getPos(String str, int num) {
+	int pos = 0;
+	for (int i = 0; i < num; i++) {
+	    pos = str.indexOf(")", pos + 1);
+	}
+	return pos;
+    }
+
 }
