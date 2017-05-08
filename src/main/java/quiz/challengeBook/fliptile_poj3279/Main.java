6e735f17ac8cf067e4c943cb75662d1a8056978f
nannany <ymym1990ymym@gmail.com>
2017-05-08 22:57:07 +0900

    準備

---
+++ src/main/java/quiz/challengeBook/fliptile_poj3279/Main.java
@@ -0,0 +1,14 @@
+package quiz.challengeBook.fliptile_poj3279;
+
+import java.util.Scanner;
+
+public class Main {
+    static int M;
+    static int N;
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	M = sc.nextInt();
+	N = sc.nextInt();
+    }
+}
