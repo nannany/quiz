8b5c924a2a0b4f63d151956a7da57cfe6ea096e2
nannany <ymym1990ymym@gmail.com>
2017-05-10 22:30:22 +0900

    二分探索upperbound、lowerboundのutil

---
+++ src/main/java/util/BinarySearchUtil.java
@@ -0,0 +1,88 @@
+package util;
+
+public class BinarySearchUtil {
+    // インスタンスは作らせない。
+    private BinarySearchUtil() {
+    };
+
+    static int[] intArrayTarget;
+    static int targetNum;
+
+    /**
+     * int配列から特定の値を二分探索で求める。 targetは昇順ソートされている必要がある。
+     * targetNumより大きい値がはじめて出現するindexを返す。
+     *
+     * @param target
+     * @param targetNum
+     * @return start int
+     */
+    static public int upperBound(int[] target, int targetNum) {
+	intArrayTarget = target;
+	BinarySearchUtil.targetNum = targetNum;
+	int start = 0;
+	int end = target.length - 1;
+
+	while (start <= end) {
+	    int mid = (start + end) / 2;
+	    if (cForUpper(mid)) {
+		start = mid + 1;
+	    } else {
+		end = mid - 1;
+	    }
+	}
+	return start;
+    }
+
+    /**
+     * int配列から特定の値を二分探索で求める。 targetは昇順ソートされている必要がある。
+     * targetNum以上の値が初めて出現するindexを返す。
+     *
+     * @param target
+     * @param targetNum
+     * @return start int
+     */
+    static public int lowerBound(int[] target, int targetNum) {
+	intArrayTarget = target;
+	BinarySearchUtil.targetNum = targetNum;
+	int start = 0;
+	int end = target.length - 1;
+
+	while (start <= end) {
+	    int mid = (start + end) / 2;
+	    if (cForLower(mid)) {
+		start = mid + 1;
+	    } else {
+		end = mid - 1;
+	    }
+	}
+	return start;
+    }
+
+    /**
+     * upperBoundにおける二分探索判定用に用いる。
+     *
+     * @param mid
+     * @return boolean
+     */
+    static private boolean cForUpper(int mid) {
+	if (intArrayTarget[mid] <= BinarySearchUtil.targetNum) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    /**
+     * lowerBoundにおける二分探索判定用に用いる。
+     *
+     * @param mid
+     * @return boolean
+     */
+    static private boolean cForLower(int mid) {
+	if (intArrayTarget[mid] < BinarySearchUtil.targetNum) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+}
