ef977102d23ce84e8232fba37571a2ca807ddd8c
nannany <ymym1990ymym@gmail.com>
2017-06-12 22:16:04 +0900

    牛歩

--- src/main/java/quiz/challengeBook/transferringSylla_poj3713/Main.java
+++ src/main/java/quiz/challengeBook/transferringSylla_poj3713/Main.java
@@ -1,5 +1,14 @@
 package quiz.challengeBook.transferringSylla_poj3713;
 
-public class Main {
+import java.util.Scanner;
 
+// 全ての頂点において、互いの最小カットが3以上であればよい
+public class Main {
+    static int N;
+    static int M;
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	N = sc.nextInt();
+	M = sc.nextInt();
+    }
 }
