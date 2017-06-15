d71e428440d54847fc1d50a046e0f799abbd0826
nannany <ymym1990ymym@gmail.com>
2017-06-15 22:31:35 +0900

    二部グラフ最大マッチング。accepted。

--- src/main/java/quiz/challengeBook/thePerfectStall_poj1274/Main.java
+++ src/main/java/quiz/challengeBook/thePerfectStall_poj1274/Main.java
@@ -10,19 +10,31 @@ public class Main {
     static int M;
     static ArrayList<Integer>[] G;
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	N = sc.nextInt();
-	M = sc.nextInt();
-
-	for (int i = 1; i <= N; i++) {
-	    G[i] = new ArrayList<Integer>();
-	    int si = sc.nextInt();
-	    for (int j = 0; j < si; j++) {
-		G[i].add(sc.nextInt());
+	while (true) {
+	    N = sc.nextInt();
+	    M = sc.nextInt();
+	    G = new ArrayList[N + M];
+	    for (int i = 0; i < N + M; i++) {
+		G[i] = new ArrayList<Integer>();
 	    }
-	}
+	    for (int i = 0; i < N; i++) {
+		int si = sc.nextInt();
+		for (int j = 0; j < si; j++) {
+		    int tmp = sc.nextInt() + (N - 1);
+		    G[i].add(tmp);
+		    G[tmp].add(i);
+		}
+	    }
 
+	    match = new int[N + M];
+	    used = new boolean[N + M];
+
+	    System.out.println(bipartiteMatching());
+	}
+//	sc.close();
     }
 
     static int[] match;
@@ -31,7 +43,34 @@ public class Main {
     static boolean dfs(int v) {
 	used[v] = true;
 	for (int i = 0; i < G[v].size(); i++) {
-	    // int u = G[v].
+	    int u = G[v].get(i);
+	    int w = match[u];
+
+	    if (w < 0 || !used[w] && dfs(w)) {
+		match[v] = u;
+		match[u] = v;
+		return true;
+	    }
 	}
+	return false;
+    }
+
+    static int bipartiteMatching() {
+	int res = 0;
+	for (int i = 0; i < N + M; i++) {
+	    match[i] = -1;
+	}
+
+	for (int v = 0; v < N + M; v++) {
+	    if (match[v] < 0) {
+		for (int i = 0; i < N + M; i++) {
+		    used[i] = false;
+		}
+		if (dfs(v)) {
+		    res++;
+		}
+	    }
+	}
+	return res;
     }
 }
