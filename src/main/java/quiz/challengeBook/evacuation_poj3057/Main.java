cd58aed24d0e51d9368135ae07dc8771ef1e34c7
nannany <ymym1990ymym@gmail.com>
2017-06-23 23:36:11 +0900

    junbi

---
+++ src/main/java/quiz/challengeBook/evacuation_poj3057/Main.java
@@ -0,0 +1,11 @@
+package quiz.challengeBook.evacuation_poj3057;
+
+import java.util.Scanner;
+
+public class Main {
+    static int X, Y;
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+    }
+}
