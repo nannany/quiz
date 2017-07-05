0b4b3adc26f00cb861bbe53130016c6fe1688df7
nannany <ymym1990ymym@gmail.com>
2017-07-06 00:09:59 +0900

    とりあえず

---
+++ src/main/java/quiz/challengeBook/intervals_poj3680/Main.java
@@ -0,0 +1,13 @@
+package quiz.challengeBook.intervals_poj3680;
+
+import java.util.Scanner;
+
+public class Main {
+    static int N, K;
+    static int[] a, b, w;
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+    }
+}
