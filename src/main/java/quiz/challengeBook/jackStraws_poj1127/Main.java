cf957b72652621a7dde3504b2649a2ad39a6f694
nannany <ymym1990ymym@gmail.com>
2017-07-09 00:58:58 +0900

    途中

--- src/main/java/quiz/challengeBook/jackStraws_poj1127/Main.java
+++ src/main/java/quiz/challengeBook/jackStraws_poj1127/Main.java
@@ -1,7 +1,52 @@
 package quiz.challengeBook.jackStraws_poj1127;
 
 public class Main {
-    static int n,m;
+    static final double EPS = 1e-10;
+
+    static double add(double a, double b) {
+	if (Math.abs(a + b) < EPS * (Math.abs(a) + Math.abs(b))) {
+	    return 0;
+	}
+	return a + b;
+    }
+
+    static class P {
+	double x, y;
+
+	public P() {
+	};
+
+	public P(double x, double y) {
+	    this.x = x;
+	    this.y = y;
+	}
+
+	P plus(P p) {
+	    return new P(add(this.x, p.x), add(this.y, p.y));
+	}
+
+	P minus(P p) {
+	    return new P(add(this.x, -p.x), add(this.y, -p.y));
+	}
+
+	P multi(double d) {
+	    return new P(this.x * d, this.y * d);
+	}
+
+	// 内積
+	double dot(P p) {
+	    return add(x * p.x, y * p.y);
+	}
+
+	// 外積
+	double det(P p) {
+	    return add(x * p.y, -y * p.x);
+	}
+    }
+
+
+
+    static int n, m;
 
     public static void main(String[] args) {
 
