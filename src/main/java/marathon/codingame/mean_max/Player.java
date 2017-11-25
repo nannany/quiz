199b4b510888c3df68070998bc39cb23d85608c3
nannany <ymym1990ymym@gmail.com>
2017-11-25 15:45:43 +0900

    途中経過

--- src/main/java/marathon/codingame/mean_max/Player.java
+++ src/main/java/marathon/codingame/mean_max/Player.java
@@ -9,7 +9,18 @@ import java.util.Scanner;
  **/
 class Player {
     static ArrayList<Wreck> wreckList;
-    static Reaper myReaper;
+    static ArrayList<Tanker> tankerList;
+    static ArrayList<Tar> tarList;
+    static ArrayList<Oil> oilList;
+    static Looter myReaper;
+    static Looter enemyReaper1;
+    static Looter enemyReaper2;
+    static Looter myDestroyer;
+    static Looter enemyDestroyer1;
+    static Looter enemyDestroyer2;
+    static Looter myDoof;
+    static Looter enemyDoof1;
+    static Looter enemyDoof2;
 
     public static void main(String args[]) {
 	Scanner in = new Scanner(System.in);
@@ -21,13 +32,18 @@ class Player {
 	    int enemyScore1 = in.nextInt();
 	    // 敵2のスコア
 	    int enemyScore2 = in.nextInt();
+	    // 俺のレイジ
 	    int myRage = in.nextInt();
-
+	    // 敵1のレイジ
 	    int enemyRage1 = in.nextInt();
+	    // 敵2のレイジ
 	    int enemyRage2 = in.nextInt();
 
 	    int unitCount = in.nextInt();
 	    wreckList = new ArrayList<>();
+	    tankerList = new ArrayList<>();
+	    tarList = new ArrayList<>();
+	    oilList = new ArrayList<>();
 	    for (int i = 0; i < unitCount; i++) {
 		int unitId = in.nextInt();
 		int unitType = in.nextInt();
@@ -41,29 +57,62 @@ class Player {
 		int extra = in.nextInt();
 		int extra2 = in.nextInt();
 
+		// reaper
 		if (unitType == 0) {
 		    if (player == 0) {
-			myReaper = new Reaper(unitId, player, mass, radius, x, y, vx, vy);
+			myReaper = new Looter(unitId, player, mass, radius, x, y, vx, vy);
 		    } else if (player == 1) {
-			Reaper enemyReaper1 = new Reaper(unitId, player, mass, radius, x, y, vx, vy);
+			enemyReaper1 = new Looter(unitId, player, mass, radius, x, y, vx, vy);
 		    } else {
-			Reaper enemyReaper2 = new Reaper(unitId, player, mass, radius, x, y, vx, vy);
+			enemyReaper2 = new Looter(unitId, player, mass, radius, x, y, vx, vy);
 		    }
+		    // destroyer
+		} else if (unitType == 1) {
+		    if (player == 0) {
+			myDestroyer = new Looter(unitId, player, mass, radius, x, y, vx, vy);
+		    } else if (player == 1) {
+			enemyDestroyer1 = new Looter(unitId, player, mass, radius, x, y, vx, vy);
+		    } else {
+			enemyDestroyer2 = new Looter(unitId, player, mass, radius, x, y, vx, vy);
+		    }
+		    // doof
+		} else if (unitType == 2) {
+		    if (player == 0) {
+			myDoof = new Looter(unitId, player, mass, radius, x, y, vx, vy);
+		    } else if (player == 1) {
+			enemyDoof1 = new Looter(unitId, player, mass, radius, x, y, vx, vy);
+		    } else {
+			enemyDoof2 = new Looter(unitId, player, mass, radius, x, y, vx, vy);
+		    }
+		    // tanker
+		} else if (unitType == 3) {
+		    Tanker tanker = new Tanker(unitId, player, mass, radius, x, y, vx, vy, extra, extra2);
+		    tankerList.add(tanker);
 		} else if (unitType == 4) {
 		    Wreck wreck = new Wreck(unitId, x, y, extra);
-		    if (wreck.water > 0) {
-			wreckList.add(wreck);
-		    } else {
-
-		    }
+		    wreckList.add(wreck);
+		} else if (unitType == 5) {
+		    Tar tar = new Tar(unitId, x, y, extra);
+		    tarList.add(tar);
+		} else if (unitType == 6) {
+		    Oil oil = new Oil(unitId, x, y, extra);
+		    oilList.add(oil);
 		}
-
 	    }
 
 	    Pos tmpPos = nearestWreckFromMyReaper(myReaper);
-	    Throttle(tmpPos.x, tmpPos.y, 300);
-	    System.out.println("WAIT");
-	    System.out.println("WAIT");
+	    // reaper操作
+	    throttle(tmpPos.x, tmpPos.y, 300);
+
+	    // destroyer操作
+
+
+	    // doof操作
+	    if (enemyScore1 > enemyScore2) {
+		throttle(enemyReaper1.x, enemyReaper1.y, 300);
+	    } else {
+		throttle(enemyReaper2.x, enemyReaper2.y, 300);
+	    }
 	}
     }
 
@@ -74,7 +123,7 @@ class Player {
      * @param y
      * @param throttle
      */
-    private static void Throttle(int x, int y, int throttle) {
+    private static void throttle(int x, int y, int throttle) {
 	System.out.println(x + " " + y + " " + throttle);
     }
 
@@ -83,7 +132,7 @@ class Player {
      *
      * @return
      */
-    private static Pos nearestWreckFromMyReaper(Reaper reaper) {
+    private static Pos nearestWreckFromMyReaper(Looter reaper) {
 	Wreck retWreck = new Wreck();
 	int distance = Integer.MAX_VALUE;
 	for (Wreck w : wreckList) {
@@ -108,8 +157,8 @@ class Player {
 	}
     }
 
-    // Reaperのクラス
-    static class Reaper {
+    // Looterのクラス
+    static class Looter {
 	int unitId;
 	int playerId;
 	float mass;
@@ -119,7 +168,7 @@ class Player {
 	int vx;
 	int vy;
 
-	public Reaper(int unitId, int playerId, float mass, int radius, int x, int y, int vx, int vy) {
+	public Looter(int unitId, int playerId, float mass, int radius, int x, int y, int vx, int vy) {
 	    super();
 	    this.unitId = unitId;
 	    this.playerId = playerId;
@@ -129,6 +178,19 @@ class Player {
 	    this.y = y;
 	    this.vx = vx;
 	    this.vy = vy;
+	}
+
+    }
+
+    // Tankerのクラス
+    static class Tanker extends Looter {
+	int water, waterCapacity;
+
+	public Tanker(int unitId, int playerId, float mass, int radius, int x, int y, int vx, int vy, int water,
+		int waterCapacity) {
+	    super(unitId, playerId, mass, radius, x, y, vx, vy);
+	    this.water = water;
+	    this.waterCapacity = waterCapacity;
 	}
 
     }
@@ -150,6 +212,29 @@ class Player {
 	    this.y = y;
 	    this.water = water;
 	}
+    }
 
+    // tar
+    static class Tar {
+	int unitId, x, y, duration;
+
+	public Tar(int unitId, int x, int y, int duration) {
+	    this.unitId = unitId;
+	    this.x = x;
+	    this.y = y;
+	    this.duration = duration;
+	}
+    }
+
+    // oil
+    static class Oil {
+	int unitId, x, y, duration;
+
+	public Oil(int unitId, int x, int y, int duration) {
+	    this.unitId = unitId;
+	    this.x = x;
+	    this.y = y;
+	    this.duration = duration;
+	}
     }
 }
