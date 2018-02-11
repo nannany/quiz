3abab9764c3ee46ea86748802f501fd98ff074c7
nannany <ymym1990ymym@gmail.com>
2018-02-12 01:35:03 +0900

    staticインポートの確かめ

---
+++ src/main/java/tryAny/staticImport.java
@@ -0,0 +1,9 @@
+package tryAny;
+
+public class staticImport {
+    public static int num = 0;
+
+    public static void print() {
+	System.out.println(num);
+    }
+}
---
+++ src/main/java/tryAny/test/Main.java
@@ -0,0 +1,10 @@
+package tryAny.test;
+
+import static tryAny.staticImport.*;
+
+public class Main {
+    public static void main(String[] args) {
+	num = 13;
+	print();
+    }
+}
