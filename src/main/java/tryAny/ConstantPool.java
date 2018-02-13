61b38bf3fecd408f54928d37a75bf263ff9a0c11
nannany <ymym1990ymym@gmail.com>
2018-02-14 00:14:18 +0900

    微修正。

--- src/main/java/tryAny/ConstantPool.java
+++ src/main/java/tryAny/ConstantPool.java
@@ -4,9 +4,9 @@ public class ConstantPool {
     public static void main(String[] args) {
 	String a1 = "constpool1";
 	String b1 = "constpool1";
-	System.out.println(a1 == b1);
+	System.out.println(a1 == b1); // true
 
-	String a2 = new String("constpool2");
+	String a2 = new String("constpool2");// false
 	String b2 = "constpool2";
 	System.out.println(a2 == b2);
     }
