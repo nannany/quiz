b851d16522a10736a2192ffc588f912d89c92428
nannany <ymym1990ymym@gmail.com>
2018-02-18 23:56:13 +0900

    更新文複数

--- src/main/java/tryAny/For.java
+++ src/main/java/tryAny/For.java
@@ -2,8 +2,22 @@ package tryAny;
 
 public class For {
     public static void main(String[] args) {
+	// 初期化文複数
 	for (int i = 0, j = 1; i < j; i++) {
 	    System.out.println(i);
 	}
+
+	// 更新文複数
+	for (int i = 0; i < 4; i++, tmp(), tmp2()) {
+	    System.out.println(i);
+	}
+    }
+
+    private static void tmp() {
+	System.out.println("tmp");
+    }
+
+    private static void tmp2() {
+	System.out.println("tmp2");
     }
 }
